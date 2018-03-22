848c4a26ccd43b239d9e90409057a07350186b7c
Vaše Jméno <vas@email.com>
2018-03-22 14:22:48 +0100

    práce

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 
     define("PI", 3.14) ;
-    $r=5 ;
+    $r=200 ;
     $S= PI*$r*$r;
     $obvod=  2*PI*$r;
  ?>
@@ -18,7 +18,7 @@
  <p> Obsah kruhu : <?php echo "S = PI * r * r =  " . PI . " * $r * $r ="  ?> <?php echo $S ?> </p>
  <p> Obvod kruhu : O = <?php echo "2 * PI * r =  2 * " . PI . " * $r " ?> = <?php echo $obvod ?></p>
 
- <div style="width":40px ; height:40px ; border-radius:20px ;
-   
+ <div style="width:<?php echo $r?>px ;display:block ;background:#F00  ; height:<?php echo $r?>px ; border-radius:<?php echo $r?>px" > </div>
+
   </body>
 </html>
